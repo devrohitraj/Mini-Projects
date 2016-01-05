d2b33687658ac0d3262dc5ea21ecbadaeaed896a
Rohit Raj <devrohitraj@users.noreply.github.com>
2016-01-05 09:27:14 +0530

    Create Replace.java

---
+++ Replace.java
@@ -0,0 +1,30 @@
+package Assignment1;
+
+public class Replace {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = args[0];
+		
+		for(int i = 0; i < name.length();i++){
+			for(int j = i + 1; j < name.length(); j++){
+				char chr1 = name.charAt(i);
+				char chr2 = name.charAt(j);
+				
+				
+				if(chr1 == chr2){
+					name = name.replace(chr2, '');
+					
+					
+				}
+			}
+		}
+		
+
+	}
+
+}
