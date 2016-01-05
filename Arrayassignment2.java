1ee839bd9aefb37e02bd9ebec6a9623f0d08f644
Rohit Raj <devrohitraj@users.noreply.github.com>
2016-01-05 14:49:03 +0530

    Update Arrayassignment2.java

--- Arrayassignment2.java
+++ Arrayassignment2.java
@@ -13,11 +13,21 @@ public class Arrayassignment2 {
 	 * @param args
 	 */
 	static int[][] numbers = new int[3][3];
+	static int[] givenNumbers = new int[4];
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		if(args.length != 4){
 			System.out.println("Please enter 4 integer numbers");
+			
+		}
+		else{
+		
+		for(int i = 0; i < 4; i++){
+			String givenvalue = args[i];
+	        System.out.println(givenvalue);
+	        givenNumbers[i] = Integer.parseInt(givenvalue);
+	        
 		}
 		
 		
@@ -26,8 +36,10 @@ public class Arrayassignment2 {
 		for(int i = 0; i < 2; i++){
 			for(int j = 0; j<2; j++){
 				numbers[i][j] = Integer.parseInt(args[k++]);
-				System.out.print(numbers[i][j]+"\t");
+				int m  = numbers[i][j];
+				System.out.print(m+"\t");
 			}
+			
 		    System.out.println(" ");
 		}
 		
@@ -35,12 +47,12 @@ public class Arrayassignment2 {
 		
 		for(int i = 1; i >= 0; i--){
 			for(int j = 1; j>=0; j--){
-			
-				System.out.print(numbers[i][j]+"\t");
+				int n = numbers[i][j];
+				System.out.print(n+"\t");
 			}
 		    System.out.println(" ");
 
 	}
-
+		}
 	}
 }
